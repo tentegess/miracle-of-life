63c042c93557cc1d9f40690a0fa31f2766062083
kBaginskik <korniszonba@gmail.com>
2023-12-06 00:49:50 +0100

    amon ending

--- assets/models/amon/amon_counter.gd
+++ assets/models/amon/amon_counter.gd
@@ -3,6 +3,9 @@ extends Node
 signal unlock
 signal lock
 
+@export var required_crosses = 9
+var is_not_active = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,17 +17,19 @@ func _process(delta):
 
 
 func action():
-	var amon = get_child(0)
 	var all_required_cross = []
 	for row in range(0,5):
 		for slot in range(0,5):
-			all_required_cross.append(amon.get_child(row).get_child(slot).get_child(0))
+			var amon = get_child(0).get_child(row).get_child(slot).get_child(0)
+			if "amon_required" in amon.get_groups():
+				all_required_cross.append(amon)
 	var cross_counter = 0
 	for cross in all_required_cross:
 		if cross.return_correct():
 			cross_counter += 1
-	if cross_counter == 9:
+	if cross_counter == required_crosses and !is_not_active:
 		unlock.emit()
-	else:
+		is_not_active = !is_not_active
+	elif is_not_active:
 		lock.emit()
-	print(cross_counter)
+		is_not_active = !is_not_active
